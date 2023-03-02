bf88b985c95b4c4a1ff148c0bc8867ca52df9e2d
Lucas Araujo <lucascaraujo@outlook.com>
2023-03-02 10:49:53 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -81,8 +81,20 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void deleDeleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM seller	WHERE Id = ?");
+			st.setInt(1, id);
 
+			int rows = st.executeUpdate();
+			if (rows == 0) {
+				throw new DbException("Error! Id entered does not exist!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
